9128529c008eea5d3cd34838aa261c1024d606c8
Michael Ahlers <michael@ahlers.co>
2017-04-16 14:27:26 -0500

    Add a cache-busting digest

--- src/main/resources/webpack.config.js
+++ src/main/resources/webpack.config.js
@@ -4,6 +4,13 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = require('./scalajs.webpack.config');
 
-module.exports.plugins = [
-  new HtmlWebpackPlugin({title: 'Sandbox: Scala.js'})
-];
+Object.assign(module.exports, {
+  plugins: [
+    new HtmlWebpackPlugin({title: 'Sandbox: Scala.js'})
+  ]
+});
+
+Object.assign(module.exports.output, {
+  filename: '[name]-[hash].js',
+  chunkFilename: '[id]-[hash].chunk.js'
+});
